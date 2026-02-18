31805dddb3f61215963657363d0a44c4974c1548
LElTEDEV <zinenizi@gmail.com>
2026-02-17 22:52:05 -0300

    feat: criado component de input

---
+++ src/components/input/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 48,
+    width: "100%",
+
+    borderRadius: 8,
+    paddingHorizontal: 16,
+
+    color: "#74798B",
+    backgroundColor: "#FFFFFF",
+
+    borderWidth: 1,
+    borderColor: "#C3C5CB",
+  },
+});
