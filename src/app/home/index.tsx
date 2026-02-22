8b84c71739fb77730d0b6e22b70fafed35b5778a
LElTEDEV <zinenizi@gmail.com>
2026-02-22 17:18:28 -0300

    feat: adicionado funcao para interagir com o async storage

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,5 +1,12 @@
-import { useState } from "react";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import { Item } from "@/components/item";
 import { Input } from "@/components/input";
@@ -8,59 +15,94 @@ import { Filter } from "@/components/filter";
 import { FilterStatus } from "@/@types/filterStatus";
 
 import { styles } from "./styles";
+import {
+  addItemStorage,
+  clearAllItemStorage,
+  getItemsStorage,
+  ItemStorage,
+  removeItemStorage,
+  updateStatusItemStorage,
+} from "@/storage/items-storage";
+
+// [
+//   { id: 1, description: "Café", status: FilterStatus.PENDING },
+//   { id: 2, description: "Leite", status: FilterStatus.DONE },
+//   { id: 3, description: "Pão", status: FilterStatus.PENDING },
+//   { id: 4, description: "Manteiga", status: FilterStatus.DONE },
+//   { id: 5, description: "Ovos", status: FilterStatus.PENDING },
+//   { id: 6, description: "Frutas", status: FilterStatus.DONE },
+//   { id: 7, description: "Verduras", status: FilterStatus.PENDING },
+//   { id: 8, description: "Carnes", status: FilterStatus.DONE },
+//   { id: 9, description: "Bebidas", status: FilterStatus.PENDING },
+//   { id: 10, description: "Doces", status: FilterStatus.DONE },
+// ];
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 
 export function Home() {
   const [activeFilter, setActiveFilter] = useState(FilterStatus.PENDING);
   const [description, setDescription] = useState("");
-  const [items, setItems] = useState([
-    { id: 1, description: "Café", status: FilterStatus.PENDING },
-    { id: 2, description: "Leite", status: FilterStatus.DONE },
-    { id: 3, description: "Pão", status: FilterStatus.PENDING },
-    { id: 4, description: "Manteiga", status: FilterStatus.DONE },
-    { id: 5, description: "Ovos", status: FilterStatus.PENDING },
-    { id: 6, description: "Frutas", status: FilterStatus.DONE },
-    { id: 7, description: "Verduras", status: FilterStatus.PENDING },
-    { id: 8, description: "Carnes", status: FilterStatus.DONE },
-    { id: 9, description: "Bebidas", status: FilterStatus.PENDING },
-    { id: 10, description: "Doces", status: FilterStatus.DONE },
-  ]);
+  const [items, setItems] = useState<ItemStorage[]>([]);
 
-  function handleStatusItem(id: number) {
-    setItems((prev) =>
-      prev.map((item) =>
-        item.id === id
-          ? {
-              ...item,
-              status:
-                item.status === FilterStatus.DONE
-                  ? FilterStatus.PENDING
-                  : FilterStatus.DONE,
-            }
-          : item,
-      ),
-    );
+  async function getStorage() {
+    try {
+      const storage = await getItemsStorage();
+
+      setItems(storage);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível visualizar os items.");
+    }
   }
 
-  function handleRemove(id: number) {
-    setItems((prev) => prev.filter((item) => item.id !== id));
+  async function handleStatusItem(item: ItemStorage) {
+    try {
+      await updateStatusItemStorage(
+        item,
+        item.status === FilterStatus.DONE
+          ? FilterStatus.PENDING
+          : FilterStatus.DONE,
+      );
+
+      await getStorage();
+    } catch (error) {
+      Alert.alert("Erro", "Erro ao tentar atualizar o status do item.");
+    }
   }
 
-  function handleClearList() {
-    setItems([]);
+  async function handleRemove(item: ItemStorage) {
+    try {
+      await removeItemStorage(item);
+      await getStorage();
+    } catch (error) {
+      Alert.alert("Erro", "Erro ao tentar remover o item da lista de compra.");
+    }
   }
 
-  function handleAddItem() {
+  async function handleAddItem() {
     if (description.length <= 0) return;
 
-    setItems((prev) => [
-      ...prev,
-      { id: prev.length + 1, description, status: FilterStatus.PENDING },
-    ]);
+    await addItemStorage({
+      id: String(Math.random()),
+      description,
+      status: FilterStatus.PENDING,
+    });
 
+    await getStorage();
     setDescription("");
   }
+
+  async function clearAllItems() {
+    try {
+      await clearAllItemStorage();
+      await getStorage();
+    } catch (error) {
+      Alert.alert("Erro", "Erro ao excluir todos os itens do seu carrinho.");
+    }
+  }
+
+  useEffect(() => {
+    getStorage();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -86,10 +128,7 @@ export function Home() {
             />
           ))}
 
-          <TouchableOpacity
-            style={styles.clearButton}
-            onPress={handleClearList}
-          >
+          <TouchableOpacity style={styles.clearButton} onPress={clearAllItems}>
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
@@ -100,8 +139,8 @@ export function Home() {
           renderItem={({ item }) => (
             <Item
               data={item}
-              onRemove={() => handleRemove(item.id)}
-              onStatus={() => handleStatusItem(item.id)}
+              onRemove={() => handleRemove(item)}
+              onStatus={() => handleStatusItem(item)}
             />
           )}
           showsVerticalScrollIndicator={false}
